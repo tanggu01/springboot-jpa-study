cad98f57211bdb6ea35c49523c4b88561b6a5915
tanggu01 <tanggu1221@gmail.com>
2022-07-05 21:15:49 +0900

    jpa test

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+
+    @Test
+    @Transactional //Spring 꺼 쓰기 . @Transactional 이 Test 에 있으면 끝나고 디비를 롤백.
+    @Rollback(false)
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member); //저장한것과 조회한거는 같다
+        System.out.println("findMember == member = " + (findMember == member));
+        //같은 transaction 안에서 저장, 조회 하면 영속성 컨텍스트가 같다.
+        //같은 영속성 컨텍스트 안에선 id 값이 같으면 같은 Entity로 식별된다.
+        //1차캐시에서 꺼내옴: select 쿼리도 안나간다.
+    }
+}
