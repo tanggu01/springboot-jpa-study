d2668707e9252bf60e50c284a6ac02ecb9ce46ec
tanggu01 <tanggu1221@gmail.com>
2022-07-08 14:06:45 +0900

    add MemberRepository

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext  //JPA
+    private EntityManager em; //주입받음
+
+    public void save(Member member) {
+        em.persist(member); //tx commit 되는시점에 DB에 반영
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class) //jpql, From 의 대상이 엔티티
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
