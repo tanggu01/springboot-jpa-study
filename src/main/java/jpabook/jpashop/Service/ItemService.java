2e97786d311909eb66d9b95d24955e0f9e6fc694
tanggu01 <tanggu1221@gmail.com>
2022-07-10 02:34:37 +0900

    update Order, OrderItem entity

--- src/main/java/jpabook/jpashop/Service/ItemService.java
+++ src/main/java/jpabook/jpashop/Service/ItemService.java
@@ -1,4 +1,4 @@
-package jpabook.jpashop.repository;
+package jpabook.jpashop.Service;
 
 import jpabook.jpashop.domain.Item.Item;
 import jpabook.jpashop.repository.ItemRepository;
@@ -12,13 +12,16 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ItemService {
     private final ItemRepository itemRepository;
+
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
     }
+
     public List<Item> findItems() {
         return itemRepository.findAll();
     }
+
     public Item findOne(Long itemId) {
         return itemRepository.findOne(itemId);
     }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -62,4 +62,45 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //==주문 생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel(); //재고 원복
+        }
+    }
+
+    //==조회 로직==//
+    /**
+     *  전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice(); //주문할때 주문가격과 수량을 곱해야하기때문
+        }
+        return totalPrice;
+    }
+
 }
